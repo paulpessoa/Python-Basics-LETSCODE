cbb6a040b53775d5fc688c1979c3a50480bfd3a8
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2021-10-28 22:50:42 -0300

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 5
+
+
+# In[3]:
+
+
+print (x)
+
+
+# In[4]:
+
+
+print(type(x))
+
+
+# In[5]:
+
+
+preco = 19.99
+print (preco, type(preco))
+
+
+# In[10]:
+
+
+cidade = "Quixada"
+print(cidade, type(cidade))
+
+
+# In[11]:
+
+
+disponivel = True
+print(disponivel, type(disponivel))
+
+
+# In[12]:
+
+
+disponivel = False
+print(disponivel, type(disponivel))
+
+
+# In[ ]:
+
+
+
+
