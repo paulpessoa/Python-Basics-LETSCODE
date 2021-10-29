8c46fef072ac077af477f0342df1e3c75922cf98
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2021-10-28 23:04:41 -0300

    operadores

---
+++ Operadores.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+x = 50
+y = 2
+
+
+# In[11]:
+
+
+print(x + y) 
+print(x * y)
+print(x - y)
+print(x / y)
+
+
+# In[12]:
+
+
+print(x ** y)
+print(x // y)
+print(x % y)
+
+
+# In[13]:
+
+
+tem_cafe = True
+tem_pao = False
+
+
+# In[14]:
+
+
+print(not tem_cafe)
+print(tem_cafe or tem_pao)
+print(tem_cafe and tem_pao)
+
+
+# In[20]:
+
+
+dolar = 5.3
+real = 1
+
+
+# In[22]:
+
+
+print(dolar > real)
+print(dolar < real)
+print(dolar >= real)
+print(dolar <= real)
+print(dolar == real)
+print(dolar != real)
+
+
+# In[ ]:
+
+
+
+
