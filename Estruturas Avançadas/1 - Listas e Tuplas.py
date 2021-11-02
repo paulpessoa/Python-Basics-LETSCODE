f173c877e109e93f595c7369ec330bf30c8c5056
Paul Pessoa <paulmspessoa@gmail.com>
2021-11-02 20:23:47 -0300

    pausa exercicio  1

---
+++ Estruturas Avançadas/1 - Listas e Tuplas.py
@@ -0,0 +1,10 @@
+nomes_paises = ['brasil', 'acanada', 'china', 'argentina', 'japao']
+print (nomes_paises)
+
+print('tamanho da lista', len(nomes_paises))
+
+print('Pais', nomes_paises[4])
+
+print('pais', nomes_paises[-1])
+nomes_paises[4] = 'Colombia'
+print(nomes_paises)
--- Estruturas Avançadas/4 - Dicionarios.py
+++
@@ -1,8 +0,0 @@
-nomes_paises = ['brasil', 'acanada', 'china', 'argentina', 'japao']
-print (nomes_paises)
-
-print('tamanho da lista', len(nomes_paises))
-
-print('Pais', nomes_paises[4])
-
-print('pais', nomes_paises[-1])
