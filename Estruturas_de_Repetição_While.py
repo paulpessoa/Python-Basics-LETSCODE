9b0c89fb159625cdc659c363d64797d2b80e07b5
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2021-11-01 23:52:52 -0300

    Update Estruturas_de_Repetição_While.py

--- Estruturas_de_Repetição_While.py
+++ Estruturas_de_Repetição_While.py
@@ -1,1 +1,48 @@
+##exemplo 1
+contador = 0
 
+while contador < 10:
+  contador += 1
+  if contador == 1:
+    print(contador, "item limpo")
+  else:
+    print(contador, "itens limpos")   
+
+print ("fim da repeticao do bloco while")
+
+
+##exemplo2
+contador = 0
+
+while True:
+  if contador < 10:
+    contador += 1
+    if contador == 1:
+      print(contador, "item limpo")
+    else:
+      print(contador, "itens limpos")
+  else:
+    break
+print ("fim da repeticao do bloco while")
+
+
+#exemplo3
+texto = input('digite sua senha: ')
+while texto != 'LetsCode':
+  texto = input('Senha inválida, Tente novamente')
+
+print('acesso permitido')
+
+
+
+#exemplo
+##exemplo 1
+contador = 0
+
+while contador < 10:
+  contador += 1
+  if contador == 1:
+    continue
+  print(contador, "itens limpos")   
+
+print ("fim da repeticao do bloco while")
