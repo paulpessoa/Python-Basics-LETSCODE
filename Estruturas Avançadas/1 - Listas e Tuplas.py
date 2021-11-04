46a05ce1daedbdfc7471a0f4f2dc093cb173f81b
Paul Pessoa <paulmspessoa@gmail.com>
2021-11-03 23:44:37 -0300

    exercicio listas e tuplas

--- Estruturas Avançadas/1 - Listas e Tuplas.py
+++ Estruturas Avançadas/1 - Listas e Tuplas.py
@@ -1,4 +1,4 @@
-nomes_paises = ['brasil', 'acanada', 'china', 'argentina', 'japao']
+nomes_paises = ['brasil', 'canada', 'china', 'argentina', 'japao']
 print (nomes_paises)
 
 print('tamanho da lista', len(nomes_paises))
@@ -8,3 +8,28 @@ print('Pais', nomes_paises[4])
 print('pais', nomes_paises[-1])
 nomes_paises[4] = 'Colombia'
 print(nomes_paises)
+
+
+print(nomes_paises[1:3])
+
+print(nomes_paises[1:-1])
+
+print(nomes_paises[2:])
+
+print(nomes_paises[:3])
+
+print(nomes_paises[:])
+
+print(nomes_paises[::2])
+
+print(nomes_paises[::-1])
+
+print(nomes_paises[::-1])
+
+print('Brasil' in nomes_paises)
+
+print('Canada' not in nomes_paises)
+
+
+lista_capitais = {}
+
