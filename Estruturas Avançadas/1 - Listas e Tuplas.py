eac9aec81c92b16e6654290a5cfbd44dc3449ea3
Paul Pessoa <paulmspessoa@gmail.com>
2021-11-06 01:44:59 -0300

    listas

--- Estruturas Avançadas/1 - Listas e Tuplas.py
+++ Estruturas Avançadas/1 - Listas e Tuplas.py
@@ -8,3 +8,24 @@ print('Pais', nomes_paises[4])
 print('pais', nomes_paises[-1])
 nomes_paises[4] = 'Colombia'
 print(nomes_paises)
+
+
+listas_capitais = []
+
+listas_capitais.append('Brasilia')
+listas_capitais.append('Buenos Aires')
+listas_capitais.append('Pequim')
+listas_capitais.append('Bogotá')
+print(listas_capitais)
+
+
+listas_capitais.insert(2, 'Paris')
+print(listas_capitais)
+
+listas_capitais.remove('Buenos Aires')
+print(listas_capitais)
+
+removido = listas_capitais.pop(2)
+print(listas_capitais, removido)
+
+
